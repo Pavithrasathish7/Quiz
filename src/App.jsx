07c9607ab4784820c5b4319c305d2091ca865ee7
Pavithra <159519447+Pavithrasathish7@users.noreply.github.com>
2024-03-05 13:04:19 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./componets/Header.jsx";
+import Quiz from "./componets/Quiz.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
+}
+
+export default App;
